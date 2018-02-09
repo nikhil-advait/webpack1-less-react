c650265e0ccf537036ece4e9ee3f7c4cc3a7264c
nikhil yeole <nikhil.yeole1@gmail.com>
2018-02-09 21:06:55 +0530

    add suport for less

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 require('./App.css');
+require('./abc.less');
 
-export default () => <h1>Hello World</h1>;
+export default () => <h1 className="some-class">Hello World</h1>;
 
 /*
 export default class App extends React.Component {
